c0d31b2e509f1eda570784dd93268e033fdeddd9
Jaime <jaimeardp@gmail.com>
2021-07-29 23:37:43 -0500

    source code: added comment helper

--- generatekeys/core/helper.py
+++ generatekeys/core/helper.py
@@ -2,10 +2,16 @@ import os
 
 
 def get_current_path_abs():
+    """
+        get current path
+    """
     path = os.path.abspath(os.getcwd())
     return path
 
 def create_folder_temp(new_folder):
+    """
+        create folder if not exist
+    """
     try:
         path_formated = os.path.join(get_current_path_abs(), new_folder)
         os.mkdir(path_formated)
@@ -15,5 +21,8 @@ def create_folder_temp(new_folder):
 
 
 def format_path_abs(base_path, filename):
+    """
+        format and concat parent path with child (or filename) path
+    """
     path_formated = os.path.join(base_path, filename)
     return path_formated
